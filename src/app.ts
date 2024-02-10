61d0f408501fdeb0e0cfc50c9991a48fcabea7af
Mike Marquezini <mike.santosmarquezini@ab-inbev.com>
2024-02-10 13:01:54 -0300

    :sparkles: feat: CRUD Api To-Do List

---
+++ src/app.ts
@@ -0,0 +1,64 @@
+import bodyParser = require('body-parser');
+import cors = require('cors');
+import { Application, Request, Response } from 'express';
+import express = require('express');
+
+const app: Application = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let tarefas: { id: number; titulo: string }[] = [];
+
+app.get('/tarefas', (req: Request, res: Response) => {
+  res.json(tarefas);
+});
+
+app.get('/tarefas/:id', (req: Request, res: Response) => {
+  res.json(tarefas);
+});
+
+app.post('/tarefas', (req: Request, res: Response) => {
+  const { titulo } = req.body;
+  const novaTarefa = { id: tarefas.length + 1, titulo };
+  tarefas.push(novaTarefa);
+  res.status(201).json(novaTarefa);
+});
+
+
+//?TODO = revisar - contêm Bug
+app.put('/tarefas/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { titulo } = req.body;
+
+  const procureTarefa = tarefas.find((taref) => taref.id === parseInt(id, 10));
+
+  if (procureTarefa) {
+    procureTarefa.titulo = titulo;
+    res.json(procureTarefa);
+  }
+  res.status(404).json({ message: 'Erroou! Tarefa inexistente parceiro' });
+});
+
+
+//TODO = revisar - contêm Bug
+app.delete('/tarefas/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const pesquisarOID = tarefas.findIndex(
+    (taref) => taref.id === parseInt(id, 1),
+  );
+
+  if (pesquisarOID !== -1) {
+    const [deleteOId] = tarefas.splice(pesquisarOID, 10);
+    res.json(deleteOId);
+  }
+  res.status(404).json({
+    message:
+      'Erroou! Parece que vai precisar pesquisar a lista para ver os elementos',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando: ✔️ `);
+});
